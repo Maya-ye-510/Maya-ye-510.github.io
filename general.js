fac2c608f6d9b89f68266e4632ae8f0cdac61ccc
Maya-ye-510 <yez216@newschool.edu>
2024-11-19 16:09:59 -0500

    Update general.js

--- general.js
+++ general.js
@@ -1,4 +1,5 @@
 const circle = document.querySelector(".circle");
+/*
 var nClicks = 0;
 
 circle.addEventListener('click', function() {
@@ -10,15 +11,18 @@ circle.addEventListener('click', function() {
    }
    nClicks = nClicks + 1;
 });
-
+*/
 const audioControl = event => {
   const clicked = event.target;
   const mp3 = clicked.previousElementSibling;
-
+  mp3.loop = true;
+   
   if (!mp3.paused) {
     mp3.pause();
+    circle.classList.remove('rotating');
   } else if (!mp3.playing) {
     mp3.play();
+    circle.classList.add('rotating');
   } else {
     return false;
   }
