928694289da80eec1e10f2675a46c7b58a2de8b0
Maya-ye-510 <yez216@newschool.edu>
2024-12-01 02:13:08 -0500

    Update pick.js

--- pick.js
+++ pick.js
@@ -1,25 +1,34 @@
 // Get the image element and button
 const imageElement = document.getElementById('pick-image');
-const slideBtn = document.getElementById('rightBtn');
+const imageBackground = document.getElementById('pick-image-background');
+const rightBtn = document.getElementById('rightBtn');
 const leftBtn = document.getElementById('leftBtn');
 
 
 // Track the current position of the image (in pixels)
-let currentPosition = 0;
+let bigCurrentPosition = 0;
+let smallCurrentPosition = 0;
+let n = 0;
 
 // Button click event
 rightBtn.addEventListener('click', () => {
   // Increment the position by 100px (or any other value you like)
-  currentPosition += 100;
+  n += 1;
+  bigCurrentPosition = n*100;
+  smallCurrentPosition = n*200;
 
   // Slide the image to the right by increasing its translateX value
-  imageElement.style.transform = `translateX(${currentPosition}px)`; // Move the image right by 'currentPosition' pixels
+  imageElement.style.transform = `translateX(${bigCurrentPosition}px)`; // Move the image right by 'currentPosition' pixels
+  imageBackground.style.transform = `translateX(${smallCurrentPosition}px)`;
 });
 
 leftBtn.addEventListener('click', () => {
   // Increment the position by 100px (or any other value you like)
-  currentPosition -= 100;
+  n -= 1;
+  bigCurrentPosition = n*100;
+  smallCurrentPosition = n*200;
 
   // Slide the image to the right by increasing its translateX value
-  imageElement.style.transform = `translateX(${currentPosition}px)`; // Move the image right by 'currentPosition' pixels
+  imageElement.style.transform = `translateX(${bigCurrentPosition}px)`; // Move the image right by 'currentPosition' pixels
+  imageBackground.style.transform = `translateX(${smallCurrentPosition}px)`;
 });
