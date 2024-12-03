9012cd70f3b54fda1bc50d1d07d8f04c0ecb7037
Maya-ye-510 <yez216@newschool.edu>
2024-12-03 00:01:46 -0500

    Update general.js

--- general.js
+++ general.js
@@ -47,8 +47,8 @@ function adjustFontSize() {
 
       // Calculate the font size based on the container's width or height
       var fontSize = containerWidth * 0.038;
-      var titleFontSize = titleWidth * 0.4; 
-      var dirFontSize = titleWidth * 0.15; 
+      var titleFontSize = titleWidth * 0.3; 
+      var dirFontSize = titleWidth * 0.06; 
 
       // Limit the font size to a minimum and maximum value
       //fontSize = Math.max(fontSize, 3);  // Minimum size of 16px
